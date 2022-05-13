349a50f1a8f491c92be74d0dace988c4a0ec95fc
BT <shiebt.1026@gmail.com>
2022-05-13 17:37:28 +0800

    新增區塊

--- js/style.js
+++ js/style.js
@@ -2,6 +2,25 @@ $(document).ready(function() {
   // 漢堡開關
   $('.burger').click(function() {
     $(this).toggleClass('open');
-    $('.navbar').toggleClass('show');
+    $('.navbar_box').slideToggle();
   });
+
+  $('a[href^="#"]').click(function(event) {
+    let $target = $(this.getAttribute('href'));
+    if ($target.length) {
+      event.preventDefault();
+      $('html,body').stop().animate({
+        scrollTop: $target.offset().top - 95
+      }, 500);
+    }
+  })
+
+  var windowWidth = $(window).width();
+  if (windowWidth < 991) {
+    $('a[href^="#"]').click(function(event) {
+      event.preventDefault();
+      $('.navbar_box').slideToggle();
+      $('.burger').toggleClass('open');
+    })
+  }
 })
